ff96cd89dae0fbd15acaf1c1cbf113afc13d6d66
Kunal Bansal <158471656+KunalBA@users.noreply.github.com>
2024-07-09 09:01:05 +0530

    Add files via upload

---
+++ HollowRectangle.java
@@ -0,0 +1,14 @@
+public class HollowRectangle {
+    public static void main(String[] args) {
+        for(int i=1;i<50;i++){
+            for(int j=1;j<51;j++){
+                if(i==1||j==1||i==49||j==50){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Palindromic.java
@@ -0,0 +1,17 @@
+public class Palindromic {
+    public static void main(String[] args) {
+        int a=4;
+        for(int i=1;i<=a;i++){
+            for(int j=1;j<=a-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=i;j>=1;j--){
+                System.out.print(j);
+            }
+            for(int j=2;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+    }
+}
